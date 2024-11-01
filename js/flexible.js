c37d6461fe906b5d4bdc2495579715970ed6a2c7
Johnny <tl071515@gmail.com>
2024-11-01 19:16:32 +0800

    0.1.1

--- js/flexible.js
+++ js/flexible.js
@@ -1,7 +1,28 @@
 (function flexible (window, document) {
   var docEl = document.documentElement
   var dpr = window.devicePixelRatio || 1
+  var offsetWid = document.documentElement.clientWidth;
+  
+   // 组件缩放处理
+   function handleComponentZoom() {
+    if (window.devicePixelRatio !== 1 && offsetWid > 478) {
+      var scaleFactor = window.devicePixelRatio; // 根据浏览器的缩放比例进行调整
+      var components = document.querySelectorAll('.zoomable'); // 假设组件有 class "zoomable"
+      var header_web = document.querySelector('.header_web');
+      var canvas = document.getElementById('canvas_sakura');
 
+      components.forEach(function(component) {
+        component.style.transform = 'scale(' + scaleFactor + ')';
+        component.style.transformOrigin = 'center'; 
+      });
+
+      if (header_web) {
+        header_web.style.transform = 'scale(' + scaleFactor + ')';
+        header_web.style.transformOrigin = 'top'; // 缩放中心从顶部开始
+        header_web.style.position = 'fixed'; // 确保其保持置顶
+      }
+    }
+  }
   // adjust body font size
   function setBodyFontSize () {
     if (document.body) {
@@ -15,6 +36,10 @@
 
   // set 1rem = viewWidth / 10
   function setRemUnit () {
+    // // !==1说明当前是缩放功能，不是浏览器窗口的变化
+    // if(window.devicePixelRatio!==1){
+    //   handleComponentZoom();  // 处理组件缩放
+    // }
     var rem = docEl.clientWidth / 10
     docEl.style.fontSize = rem + 'px'
   }
--- node.js
+++ node.js
@@ -5,12 +5,55 @@ const https = require('https');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root123',
+    database: 'mahjong'
+  });
 app.use(express.static('./'));
 
 app.get('/', (req, res) => {
     
     res.redirect('/index.html');
 });
+
+app.post('/register', (req, res)=>{
+    const { region, phone } = req.body;
+    if (!region || !phone) {
+        return res.status(500).json({ error: '参数不完整', code: 400 });
+    }
+    connection.query(
+          'SELECT * FROM user WHERE region = ? AND phone = ?',
+          [region, phone],
+          (error, results, fields) => {
+            if (error) {
+              console.error(error);
+              return res.status(500).json({ error: '提交失败,数据库查询出错', code: 400 });
+            }
+      
+            // 如果数据库中不存在相同的 region 和 phone 值,则插入新数据
+            if (results.length === 0) {
+              connection.query(
+                'INSERT INTO user (region, phone) VALUES (?, ?)',
+                [region, phone],
+                (error, results, fields) => {
+                  if (error) {
+                    console.error(error);
+                    return res.status(500).json({ error: error, code: 400 });
+                  }
+                  console.log(`收到手机号: ${phone}, 地区: ${region}`);
+                  res.json({ message: '提交成功', code: 200 });
+                }
+              );
+            } else {
+              res.json({ error: '手机号和地区已存在', code: 400 });
+            }
+          }
+        );
+  })
 
 const options = {
     key: fs.readFileSync('./key/mahjong.key'),
